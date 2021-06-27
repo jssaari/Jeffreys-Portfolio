d79ddb5dffcbff69f0b6fc32cf7f2712b0d7ba02
Jeffrey <saarijeffrey@gmail.com>
2021-06-27 11:20:04 -0700

    Adding styles to nav for current page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+const curLocation = window.location.pathname;
+const about = '/about-me.html';
+var aboutEl = document.getElementById('about-nav');
+const work = '/my-work.html';
+var workEl = document.getElementById('work-nav');
+
+about === curLocation
+  ? (aboutEl.style.opacity = 1) && (aboutEl.style.fontWeight = 700)
+  : console.log();
+work === curLocation
+  ? (workEl.style.opacity = 1) && (workEl.style.fontWeight = 700)
+  : console.log();
